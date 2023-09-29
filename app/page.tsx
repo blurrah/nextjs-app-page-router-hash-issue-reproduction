68d614bc05b3428a04ed305c647e6539c673ba4c
Boris Besemer <borisbesemer@gmail.com>
2023-09-29 15:45:56 +0200

    feat: add reproduction for losing location.hash on switching router contexts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import Link from "next/link";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <Link href="/test#blabla">
+      <p>Issue :)</p>
+    </Link>
+  );
 }
---
+++ pages/test.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <Link href="/#blabla">
+      <h1>Test</h1>
+    </Link>
+  );
+}
